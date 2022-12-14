d5adb9e100249787aa768eda5c940ef46566201c
kotovstaya <antsavinkov@gmail.com>
2022-12-14 10:04:48 +0000

    [MLE-009] fixes

--- meta/airflow/dags/pobeda_delivery.py
+++ meta/airflow/dags/pobeda_delivery.py
@@ -5,8 +5,38 @@ import datetime as dt
 
 import pendulum
 from airflow import DAG
+from airflow.contrib.operators.slack_webhook_operator import SlackWebhookOperator
+from airflow.hooks.base_hook import BaseHook
+from airflow.models import Variable
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+
+SLACK_CONN_ID = 'slack'
+def task_fail_slack_alert(context):
+    slack_webhook_token = BaseHook.get_connection(SLACK_CONN_ID).password
+    slack_msg = """
+            :red_circle: Task Failed.
+            <@U022T50D4F5>  
+            *Task*: {task}  
+            *Dag*: {dag} 
+            *Execution Time*: {exec_date}  
+            *gsutil URI*: {gsutil_URI} 
+            """.format(
+                task=context.get('task_instance').task_id,
+                dag=context.get('task_instance').dag_id,
+                ti=context.get('task_instance'),
+                exec_date=context.get('execution_date'),
+                gsutil_URI=context.get('task_instance').log_url.replace("8080", "8082")
+            )
+
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_test',
+        http_conn_id='slack',
+        webhook_token=slack_webhook_token,
+        message=slack_msg,
+        username='airflow')
+    return failed_alert.execute(context=context)
+
 
 default_args = {
     'owner': 'asavinkov',
@@ -16,10 +46,11 @@ default_args = {
     'email_on_failure': True,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': dt.timedelta(minutes=5),
+    'retry_delay': dt.timedelta(seconds=5),
+    'on_failure_callback': task_fail_slack_alert,
 }
 
-current_date = '20221125'
+current_date = Variable.get("dt")
 
 with DAG(
     dag_id="pobeda_delivery",
